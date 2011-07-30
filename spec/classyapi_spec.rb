223e18c1675a5369da5adf1791ba093e254c5fa4
seeingidog <ian@ruby-code.com>
2011-07-29 23:11:51 -0400

    initial commit, start of test

--- spec/classyapi_spec.rb
+++ spec/classyapi_spec.rb
@@ -1,7 +1,17 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+class MyStocks
+  attr_reader :my_brokerage_account
+  @my_brokerage_account = "32423423"
+  
+  def list_stock(stock_symbol)
+    { :name => stock_symbol, :price => 31.39 }
+  end  
+end
+
+
 describe "Classyapi" do
-  it "fails" do
+  it "should create" do
     fail "hey buddy, you should probably rename this file and start specing for real"
   end
 end
