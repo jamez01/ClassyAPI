8cf2adb817e1e4f2a0e1b52173d2af76cadc1f22
Ian Morgan <ian@ruby-code.com>
2011-08-02 12:32:44 -0400

    added machine readable error messages

--- lib/classyapi.rb
+++ lib/classyapi.rb
@@ -45,5 +45,39 @@ module ClassyAPI
       }
 
     end
+    
+    get "*" do
+      status 404
+    end
+
+    post "*" do
+      status 404
+    end
+
+    put "*" do
+      status 404
+    end
+
+    delete "*" do
+      status 404
+    end
+
+    not_found do
+      json_status 404, "Not found"
+    end
+
+    error do
+      json_status 500, env['sinatra.error'].message
+    end
+    
+    helpers do
+      def json_status(code, reason)
+        status code
+        {
+          :status => code,
+          :reason => reason
+        }.to_json
+      end
+    end
   end
 end
