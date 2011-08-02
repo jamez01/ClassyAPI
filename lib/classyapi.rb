4133d3036f8fe744c00223a61d56fc30081d4015
Ian Morgan <ian@ruby-code.com>
2011-08-02 12:34:09 -0400

    removed some of those last routes, still good error handling

--- lib/classyapi.rb
+++ lib/classyapi.rb
@@ -46,22 +46,6 @@ module ClassyAPI
 
     end
     
-    get "*" do
-      status 404
-    end
-
-    post "*" do
-      status 404
-    end
-
-    put "*" do
-      status 404
-    end
-
-    delete "*" do
-      status 404
-    end
-
     not_found do
       json_status 404, "Not found"
     end
