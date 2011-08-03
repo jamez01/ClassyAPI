b2573e9f166d7312653673565d66e66fd2c6ff5a
Ian Morgan <ian@ruby-code.com>
2011-08-03 11:20:32 -0400

    more specific test and spelling

--- examples/mystocks.rb
+++ examples/mystocks.rb
@@ -15,7 +15,7 @@ class MyStocks
   end
 
   def buy_stock(stock_symbol,ammount)
-    { :symbol => stock_symbol, :amount => ammount }
+    { :symbol => stock_symbol, :amount => amount }
   end
 
 end
